78bc80b6ed686b7bd3d5db5adfdd6140ad6f2d5c
Implication <IamTrajon@gmail.com>
2020-02-19 22:56:19 -0800

    Updates

---
+++ precache-manifest.b573d4da6784e1cbf23e858aff507d52.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37c4060ff749f75e70f078c573b0a542",
+    "url": "/songs/index.html"
+  },
+  {
+    "revision": "913b0ac9473a4bfe5f8c",
+    "url": "/songs/static/js/2.f8541d50.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/songs/static/js/2.f8541d50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4bdf53140983fcdf6a7",
+    "url": "/songs/static/js/main.2658dc4f.chunk.js"
+  },
+  {
+    "revision": "fc8dcd007500c0fd04ce",
+    "url": "/songs/static/js/runtime-main.91e7c810.js"
+  }
+]);
